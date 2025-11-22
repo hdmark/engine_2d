9f5b3cefd441f0ef8b71147ebfa908291cf7f944
mark baskin <mark.d.baskin@gmail.com>
2025-11-22 09:20:14 -0500

    working on rect

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::panic;
 
+use glm::{ abs, sqrt };
 use pix_engine::prelude::*;
 use nalgebra::*;
 extern crate nalgebra as na;
@@ -35,6 +36,12 @@ impl Collider {
             _ => panic!("Expected CircleE2d"),
         }
     }
+    fn as_rect(&self) -> &RectE2d {
+        match self {
+            Collider::RectE2d(c) => c,
+            _ => panic!("Expected RectE2d"),
+        }
+    }
 }
 
 pub struct Body {
@@ -57,6 +64,7 @@ impl CircleE2d {
 impl RectE2d {
     pub fn new(min: Vec2, max: Vec2, length: f32, width: f32) -> Self {
         // RectE2d { half }
+        // TODO - do pos + width /2 and pos + height /2
         RectE2d { min, max, width, length }
     }
 
@@ -100,7 +108,8 @@ pub struct Manifold<'a> {
     penetration: f32,
     normal: Vec2,
 }
-pub fn CircleVsCircle(m: &mut Manifold) -> bool {
+pub fn circle_vs_circle(m: &mut Manifold) -> bool {
+    dbg!("c v c check");
     let ca = m.a;
     let cb = m.b;
     let sa = m.a.collider.as_circle();
@@ -124,21 +133,100 @@ pub fn CircleVsCircle(m: &mut Manifold) -> bool {
 
     return true;
 }
+pub fn aabb_vs_circle(m: &mut Manifold) -> bool {
+    dbg!("r v c check");
+
+    let col_a = m.a;
+    let col_b = m.b;
+    let rect_a = m.a.collider.as_rect();
+    let circ_b = m.b.collider.as_circle();
+
+    // vec from a to b
+    let n: Vec2 = col_a.position - col_b.position;
+
+    // closest point on a to center of b
+    let mut closest: Vec2 = n;
+
+    // calc half extents
+    let x_extent = (rect_a.max.x - rect_a.min.x) / 2.0;
+    let y_extent = (rect_a.max.y - rect_a.min.y) / 2.0;
+
+    // clamp point to edges of the aabb
+    closest.x = clamp(-x_extent, x_extent, closest.x);
+    closest.y = clamp(-y_extent, y_extent, closest.y);
+
+    let mut inside = false;
+    // if circle inside, clamp center to closest edge
+    if n == closest {
+        inside = true;
+        if n.x.abs() > n.y.abs() {
+            // clamp to closest extent
+            if closest.x > 0.0 {
+                closest.x = x_extent;
+            } else {
+                closest.x = -x_extent;
+            }
+        }
+        // y axis is shorter
+    } else {
+        if closest.y > 0.0 {
+            closest.y = y_extent;
+        } else {
+            closest.y = -y_extent;
+        }
+    }
+    let normal = n - closest;
+    let mut d = normal.magnitude_squared();
+    let r = circ_b.radius;
+
+    // Early out of the radius is shorter than distance to closest point and
+    // Circle not inside the AABB
+
+    if d > r * r && !inside {
+        dbg!("early out");
+        return false;
+    }
+
+    d = d.sqrt();
+
+    // Collision normal needs to be flipped to point outside if circle was
+    // inside the AABB
+    dbg!(inside);
+    if inside {
+        m.penetration = r - d;
+        m.normal = -n;
+    } else {
+        m.penetration = r - d;
+        m.normal = n;
+    }
+
+    return true;
+}
 fn rc(a: &mut Body, b: &mut Body) {
+    let mut m = Manifold {
+        a: a,
+        b: b,
+        penetration: 0.0,
+        normal: Vec2::new(0.0, 0.0),
+    };
     let col: bool = match (&a.collider, &b.collider) {
         (Collider::CircleE2d(i), Collider::CircleE2d(j)) => {
-            let mut r = i.radius + j.radius;
-            r *= r;
-            // dbg!(r, a.position.x, b.position.x, a.position.y, b.position.y);
-            // return r > (a - b).powf(2.0);
-
-            r > (a.position.x - b.position.x).powf(2.0) + (a.position.y - b.position.y).powf(2.0)
+            // let mut r = i.radius + j.radius;
+            // r *= r;
+            // r > (a.position.x - b.position.x).powf(2.0) + (a.position.y - b.position.y).powf(2.0)
+            circle_vs_circle(&mut m)
         }
-        (Collider::CircleE2d(circle_e2d), Collider::RectE2d(rect_e2d)) => todo!(),
-        (Collider::RectE2d(rect_e2d), Collider::CircleE2d(circle_e2d)) => false,
+        (Collider::CircleE2d(circle_e2d), Collider::RectE2d(rect_e2d)) => {
+            // flip the inputs
+            m.a = b;
+            m.b = a;
+            aabb_vs_circle(&mut m)
+        }
+        (Collider::RectE2d(rect_e2d), Collider::CircleE2d(circle_e2d)) => { aabb_vs_circle(&mut m) }
         (Collider::RectE2d(i), Collider::RectE2d(j)) => todo!(),
     };
     if !col {
+        dbg!("no col");
         return;
     }
     let rv = b.velocity - a.velocity;
@@ -209,21 +297,19 @@ impl PixEngine for MyApp {
         // Set fill color to black if mouse is pressed, otherwise wite.
         s.fill(color!(0));
 
-        let r = Rect::new(0, (max_height as i32) - 20, max_width as i32, max_height as i32);
-        s.rect(r)?;
-        for i in self.objects.iter() {
-            let v = i.to_draw();
-            match &i.collider {
-                Collider::CircleE2d(c) => {
-                    s.circle([v[0], v[1], v[2]])?;
-                    // s.circle([i.position.x as i32, i.position.y as i32, c.radius as i32])?;
-                }
-                Collider::RectE2d(r) => {
-                    s.rect([v[0], v[1], v[2], v[3]])?;
-                    // s.rect(rect!([0, 0], 100, 100))?;
-                }
-            }
+        // UPDATE PHYSICS
+        if self.objects.len() >= 2 {
+            dbg!(self.objects.len());
+            let i = self.objects.len() - 2;
+            let j = self.objects.len() - 1;
+
+            let (i, j) = if i < j { (i, j) } else { (j, i) };
+            let (left, right) = self.objects.split_at_mut(j);
+
+            rc(&mut left[i], &mut right[0]);
         }
+
+        // Adjust positions
         let mut items_to_remove: Vec<usize> = Vec::new();
         for (idx, c) in self.objects.iter_mut().enumerate() {
             // grab to remove later
@@ -241,22 +327,27 @@ impl PixEngine for MyApp {
                 // dbg!(c.position.y);
             }
         }
-
-        if self.objects.len() >= 2 {
-            dbg!(self.objects.len());
-            let i = self.objects.len() - 2;
-            let j = self.objects.len() - 1;
-
-            let (i, j) = if i < j { (i, j) } else { (j, i) };
-            let (left, right) = self.objects.split_at_mut(j);
-            rc(&mut left[i], &mut right[0]);
-        }
-
         // remove items off screen
         for idx in items_to_remove.iter().rev() {
             self.objects.remove(*idx);
         }
 
+        // draw all items
+        let r = Rect::new(0, (max_height as i32) - 20, max_width as i32, max_height as i32);
+        s.rect(r)?;
+        for i in self.objects.iter() {
+            let v = i.to_draw();
+            match &i.collider {
+                Collider::CircleE2d(c) => {
+                    s.circle([v[0], v[1], v[2]])?;
+                    // s.circle([i.position.x as i32, i.position.y as i32, c.radius as i32])?;
+                }
+                Collider::RectE2d(r) => {
+                    s.rect([v[0], v[1], v[2], v[3]])?;
+                    // s.rect(rect!([0, 0], 100, 100))?;
+                }
+            }
+        }
         let m = s.mouse_pos();
         s.circle([m.x(), m.y(), 5])?;
 
@@ -313,7 +404,7 @@ fn main() -> PixResult<()> {
         restitution: 0.8,
         collider: Collider::CircleE2d(CircleE2d { radius: 200.0 }),
     };
-    app.objects.push(circle_body);
+    // app.objects.push(circle_body);
 
     engine.run(&mut app)
 }
